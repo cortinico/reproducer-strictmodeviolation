fcb282689f013ef35f26c3821b9fc3759fdd4e45
Nicola Corti <corti.nico@gmail.com>
2023-02-25 16:28:15 +0000

    Add detectLeakedClosableObjects StrictPolicy

--- app/src/main/java/com/ncorti/kotlin/template/app/MainActivity.kt
+++ app/src/main/java/com/ncorti/kotlin/template/app/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.ncorti.kotlin.template.app
 
 import android.os.Bundle
+import android.os.StrictMode
 import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        StrictMode.setVmPolicy(
+            StrictMode.VmPolicy.Builder()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .penaltyDeath()
+                .build()
+        )
     }
 }
